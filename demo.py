f98b837f7afa6d8f946bf08d3ac4af75bfc40424
duan922 <1362554825@qq.com>
2022-01-08 11:40:46 +0800

    dd

---
+++ demo.py
@@ -0,0 +1,19 @@
+import turtle as t
+t.pensize(2)
+# 笔大小2像素
+t.pencolor("red")
+# 颜色为红色
+t.left(45)
+# 45度
+t.fd(200)
+# 向前200直线
+t.circle(100, 180)
+# 画一圆半径100 弧度180
+t.right(90)
+# 向右90度
+t.circle(100, 180)
+# 再画一个圆半径100 弧度180
+t.fd(200)
+# 直线向前直线200
+t.done()
+# 绘制完成
